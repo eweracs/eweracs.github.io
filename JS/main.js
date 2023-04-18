b7fd630478a1d63c340c6231a262e021dbd6a846
Sebastian Carewe <51919489+eweracs@users.noreply.github.com>
2023-04-19 00:35:52 +0200

    Rework CSS, add JS to center content up to certain height

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+function setSpacerHeight() {
+    window.onresize = function() {
+        setSpacerHeight();
+    }
+    console.log("scrolling");
+    var spacer = document.getElementById("spacer");
+    var content = document.getElementById("content");
+    var navBar = document.getElementById("nav");
+    var navBarHeight = navBar.offsetHeight;
+
+    var middleOfScreen = window.innerHeight / 2;
+    var contentHeight = content.offsetHeight;
+    var contentMiddle = contentHeight / 2;
+    var spacerHeight = middleOfScreen - contentMiddle - navBarHeight;
+    spacer.style.height = spacerHeight + "px";
+}
