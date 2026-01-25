d510a3accbb4e2c898c9fd8f35d2d890a3718a76
Sebastian Carewe <51919489+eweracs@users.noreply.github.com>
2026-01-25 19:39:15 +0100

    Remove Shortener Service

--- shortener-service/sql/schema.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists short_links (
-  id serial primary key,
-  short_id varchar(6) unique not null,
-  drive_id text not null,
-  name text,
-  created_at timestamp with time zone default now()
-);
